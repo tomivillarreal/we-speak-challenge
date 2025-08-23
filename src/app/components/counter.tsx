94b18f33f65acb63330f93e8498ec7411851eec5
tomivillarreal <tomasvillarreal2003@outlook.com>
2025-08-23 14:28:42 -0300

    Solucion de bug al decrementar en valor 0

--- src/app/components/counter.tsx
+++ src/app/components/counter.tsx
@@ -36,6 +36,8 @@ export default function Counter({ data }: { data: Counter }) {
         } catch (error) {
             toast.error("El valor del contador no puede ser negativo");
             console.error("Error decrementando el contador:", error);
+        }finally{
+            setIsLoading(false);
         }
     }
 
@@ -72,7 +74,7 @@ export default function Counter({ data }: { data: Counter }) {
     }, []);
 
     return (
-        <Card className="p-10 flex flex-col gap-4 h-[300px] w-full sm:w-[350px] justify-center items-center">
+        <Card className="p-10 flex flex-col gap-4 h-[300px] w-full sm:w-[350px] justify-center items-center shadow-[#7e55f6] shadow-xl  ">
             <Tooltip>
                 <TooltipTrigger>
                     <Button size="icon" variant={"ghost"} className="cursor-pointer" onClick={() => startTransition(actionReset)} disabled={pendingReset}> {pendingReset ? <Spinner size={20} /> : <RotateCcw />}</Button>
